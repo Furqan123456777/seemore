ff543c6fd4147afd0802403f6a52eaa384156fdb
Furqan123456777 <158835274+Furqan123456777@users.noreply.github.com>
2024-03-31 17:23:25 +0500

    ok

---
+++ app.js
@@ -0,0 +1,33 @@
+function see_more(){
+    var c = (document.getElementById("c").style.display = "flex" )
+    var b = (document.getElementById("b").style.display = "flex" )
+    var a = (document.getElementById("a").style.display = "flex" )
+    var d = (document.getElementById("d").style.display = "flex" )
+    var e = (document.getElementById("e").style.display = "flex" )
+    var f = (document.getElementById("f").style.display = "flex" )
+    var g = (document.getElementById("g").style.display = "flex" )
+    var h = (document.getElementById("h").style.display = "flex" )
+     var btn = (document.getElementById("btn").style.display = "none")
+    var button = (document.getElementById("button").style.display = "flex")
+}
+
+
+
+
+
+
+
+
+function see_less(){
+    var a = (document.getElementById("a").style.display = "none" );
+    var b = (document.getElementById("b").style.display = "none" );
+    var c = (document.getElementById("c").style.display = "none" );
+    var d = (document.getElementById("d").style.display = "none" );
+    var e = (document.getElementById("e").style.display = "none" );
+    var f = (document.getElementById("f").style.display = "none" );
+    var g = (document.getElementById("g").style.display = "none" );
+    var h = (document.getElementById("h").style.display = "none" );
+     var btn = (document.getElementById("btn").style.display = "fle")
+    var button = (document.getElementById("button").style.display = "none");
+}
+
